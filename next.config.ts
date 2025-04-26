b060f8b0b7f30e0e0b74dff9780544873dfa9609
gashi-olti <oltiongashi21@gmail.com>
2025-04-26 23:17:14 +0200

    build: update next.config and tsconfig for improved project configuration

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  serverRuntimeConfig: {
+    baseUrl: process.env.API_URL,
+  },
+  env: {
+    NEWS_API_KEY: process.env.NEWS_API_KEY,
+  },
+  images: {
+    domains: ["dims.apnews.com"],
+  },
 };
 
 export default nextConfig;
