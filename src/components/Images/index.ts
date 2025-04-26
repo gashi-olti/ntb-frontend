d98c04b77af43df43269b8d567d5019ce0a687c6
gashi-olti <oltiongashi21@gmail.com>
2025-04-26 23:16:36 +0200

    add image assets

---
+++ src/components/Images/index.ts
@@ -0,0 +1,10 @@
+export const Images = {
+  BlackLogo: require("/public/images/native-teams-logo-black.svg"),
+  WhiteLogo: require("/public/images/native-teams-logo-white.svg"),
+  LoadingIcon: require("/public/images/loading-icon.svg"),
+  LikeIcon: require("/public/images/like-icon.svg"),
+  LoveIcon: require("/public/images/love-icon.png").default.src,
+  LoveIcon2: require("/public/images/love-icon-2.png").default.src,
+  NTLogoWhite: require("/public/images/nt-logo-white.png").default.src,
+  NTLogoDefault: require("/public/images/nt-logo-default.svg"),
+};
